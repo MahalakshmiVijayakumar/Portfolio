55e0d164dd00c938adda498bda12cba650dc4131
MahalakshmiVijayakumar <168769673+MahalakshmiVijayakumar@users.noreply.github.com>
2025-05-19 21:32:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var Typed= new Typed(".text", {
+    strings:["Frontend Developer", "Web Developer", "Python with Django Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true 
+})
